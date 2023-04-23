6e35011e2d03d37ec170575d50bec6d167802b52
Neta Elad <elad.neta@gmail.com>
2023-04-23 19:19:03 +0300

    ignore git and github files

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ setmetatable(obj, obj)
 
 -- Metadata
 obj.name = "DocsGenerator"
-obj.version = "0.0.8"
+obj.version = "0.0.9"
 obj.author = "Neta Elad <elad.neta@gmail.com>"
 obj.homepage = "https://github.com/netaelad/DocsGenerator.spoon"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
